770cdc155461e4463f92cd85f6036f6715bb3b49
Nagashima-202201 <97273987+Nagashima-202201@users.noreply.github.com>
2022-02-24 14:26:11 +0900

    課題提出
    PHP＞2章＞foreach

---
+++ 2-8/index.php
@@ -0,0 +1,9 @@
+<?php
+$fruits = ["apple" => "りんご", "orange" => "オレンジ", "peach" => "もも"];
+foreach($fruits as $key => $value){
+    echo $key;
+    echo "といったら";
+    echo $value;
+    echo '<br>';
+}
+?>
