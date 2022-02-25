7f74e74bed24a3c1323ff3946d5feb66d9aa43ac
Nagashima-202201 <97273987+Nagashima-202201@users.noreply.github.com>
2022-02-25 10:48:50 +0900

    課題提出_2
    PHP＞2章＞配列、連想配列

--- 2-7/index.php
+++ 2-7/index.php
@@ -2,6 +2,6 @@
 $color = ["red" => "赤", "blue" => "青", "green" => "緑"];
 var_dump($color);
 echo '<br>';
-$colors = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
-var_dump($colors);
+$color["yellow"] = "黄色";
+var_dump($color);
 ?>
